809b11ea895fcf485f65a90dc1cca460cf698081
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2020-12-25 15:42:08 +0300

    lazy loadin for courses module

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-
+  {
+    path: '',
+    component: HomeComponent,
+  }
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forChild(routes),
   ],
   exports: [RouterModule],
   providers: [
 
   ]
 })
-export class CoursesRoutingModule {
-
-
-
-}
+export class CoursesRoutingModule {}
